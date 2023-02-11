743b81176001c81acad62a077fbd066888dd4f42
Ismaiil Kazanfarov <ismaiilka7@ya.ru>
2023-02-11 13:37:57 +0300

    Version 2

--- Fraction.py
+++ Fraction.py
@@ -1,16 +1,29 @@
 class Fraction:
     """Fraction calculator"""
     def __init__(self, *args):
-        if len(args) == 1:
+        if len(args) == 1 and self._is_number(args[0]):
+            numerator = int(args[0])
+            denominator = 1
+        elif len(args) == 1:
             numerator, denominator = [int(value) for value in args[0].split('/')]
         else:
             numerator, denominator = args
+
         if not (numerator < 0 and denominator < 0) and (numerator < 0 or denominator < 0):
             self.sign = -1
         else:
             self.sign = 1
+
         self.num, self.denom = abs(numerator), abs(denominator)
         self._reduction()
+
+    @staticmethod
+    def _is_number(str):
+        try:
+            int(str)
+            return True
+        except ValueError:
+            return False
 
     @staticmethod
     def _greatest_common_divisor(a, b):
@@ -20,9 +33,6 @@ class Fraction:
             else:
                 b = b % a
         return a + b
-
-    def _smallest_common_multiple(self, a, b):
-        return a // self._greatest_common_divisor(a, b) * b
 
     def _reduction(self):
         divisor = self._greatest_common_divisor(self.num, self.denom)
@@ -55,77 +65,129 @@ class Fraction:
         return Fraction(-self.sign * self.num, self.denom)
 
     def __add__(self, other):
-        if not isinstance(other, Fraction):
+        if isinstance(other, Fraction):
+            num = self.sign * self.num * other.denom + other.sign * other.num * self.denom
+            denom = self.denom * other.denom
+        elif isinstance(other, int):
+            num = self.sign * self.num + other * self.denom
+            denom = self.denom
+        else:
             raise TypeError
-        num = self.sign * self.num * other.denom + other.sign * other.num * self.denom
-        denom = self.denom * other.denom
         return Fraction(num, denom)
+
+    def __radd__(self, other):
+        return self.__add__(other)
 
     def __sub__(self, other):
-        if not isinstance(other, Fraction):
+        if isinstance(other, Fraction):
+            num = self.sign * self.num * other.denom - other.sign * other.num * self.denom
+            denom = self.denom * other.denom
+        elif isinstance(other, int):
+            num = self.sign * self.num - other * self.denom
+            denom = self.denom
+        else:
             raise TypeError
-        num = self.sign * self.num * other.denom - other.sign * other.num * self.denom
-        denom = self.denom * other.denom
         return Fraction(num, denom)
 
+    def __rsub__(self, other):
+        return -self.__sub__(other)
+
     def __iadd__(self, other):
-        if not isinstance(other, Fraction):
+        if isinstance(other, Fraction):
+            num = self.sign * self.num * other.denom + other.sign * other.num * self.denom
+            denom = self.denom * other.denom
+        elif isinstance(other, int):
+            num = self.sign * self.num + other * self.denom
+            denom = self.denom
+        else:
             raise TypeError
-        num = self.sign * self.num * other.denom + other.sign * other.num * self.denom
-        denom = self.denom * other.denom
+
         if num < 0:
             self.sign = -1
             num *= -1
+
         self.num, self.denom = num, denom
         self._reduction()
         return self
 
     def __isub__(self, other):
-        if not isinstance(other, Fraction):
+        if isinstance(other, Fraction):
+            num = self.sign * self.num * other.denom - other.sign * other.num * self.denom
+            denom = self.denom * other.denom
+        elif isinstance(other, int):
+            num = self.sign * self.num - other * self.denom
+            denom = self.denom
+        else:
             raise TypeError
-        num = self.sign * self.num * other.denom - other.sign * other.num * self.denom
-        denom = self.denom * other.denom
+
         if num < 0:
             self.sign = -1
             num *= -1
+
         self.num, self.denom = num, denom
         self._reduction()
         return self
 
     def __mul__(self, other):
-        if not isinstance(other, Fraction):
+        if isinstance(other, Fraction):
+            num = self.sign * other.sign * self.num * other.num
+            denom = self.denom * other.denom
+        elif isinstance(other, int):
+            num = self.sign * other * self.num
+            denom = self.denom
+        else:
             raise TypeError
-        num = self.sign * other.sign * self.num * other.num
-        denom = self.denom * other.denom
         return Fraction(num, denom)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
 
     def __truediv__(self, other):
-        if not isinstance(other, Fraction):
+        if isinstance(other, Fraction):
+            num = self.sign * other.sign * self.num * other.denom
+            denom = self.denom * other.num
+        elif isinstance(other, int):
+            num = self.sign * self.num
+            denom = self.denom * other
+        else:
             raise TypeError
-        num = self.sign * other.sign * self.num * other.denom
-        denom = self.denom * other.num
         return Fraction(num, denom)
 
+    def __rtruediv__(self, other):
+        return self.__truediv__(other).reverse()
+
     def __imul__(self, other):
-        if not isinstance(other, Fraction):
+        if isinstance(other, Fraction):
+            num = self.sign * other.sign * self.num * other.num
+            denom = self.denom * other.denom
+        elif isinstance(other, int):
+            num = self.sign * other * self.num
+            denom = self.denom
+        else:
             raise TypeError
-        num = self.sign * other.sign * self.num * other.num
-        denom = self.denom * other.denom
+
         if num < 0:
             self.sign = -1
             num *= -1
+
         self.num, self.denom = num, denom
         self._reduction()
         return self
 
     def __itruediv__(self, other):
-        if not isinstance(other, Fraction):
+        if isinstance(other, Fraction):
+            num = self.sign * other.sign * self.num * other.denom
+            denom = self.denom * other.num
+        elif isinstance(other, int):
+            num = self.sign * self.num
+            denom = self.denom * other
+        else:
             raise TypeError
-        num = self.sign * other.sign * self.num * other.denom
-        denom = self.denom * other.num
+
         if num < 0:
             self.sign = -1
             num *= -1
+
         self.num, self.denom = num, denom
         self._reduction()
         return self
@@ -134,3 +196,51 @@ class Fraction:
         self.num, self.denom = self.denom, self.num
         self._reduction()
         return self
+
+    def __eq__(self, other):
+        if isinstance(other, Fraction):
+            return self.sign * self.num / self.denom == other.sign * other.num / other.denom
+        elif isinstance(other, int):
+            return self.sign * self.num / self.denom == other
+        else:
+            raise TypeError
+
+    def __ne__(self, other):
+        if isinstance(other, Fraction):
+            return self.sign * self.num / self.denom != other.sign * other.num / other.denom
+        elif isinstance(other, int):
+            return self.sign * self.num / self.denom != other
+        else:
+            raise TypeError
+
+    def __lt__(self, other):
+        if isinstance(other, Fraction):
+            return self.sign * self.num / self.denom < other.sign * other.num / other.denom
+        elif isinstance(other, int):
+            return self.sign * self.num / self.denom < other
+        else:
+            raise TypeError
+
+    def __gt__(self, other):
+        if isinstance(other, Fraction):
+            return self.sign * self.num / self.denom > other.sign * other.num / other.denom
+        elif isinstance(other, int):
+            return self.sign * self.num / self.denom > other
+        else:
+            raise TypeError
+
+    def __le__(self, other):
+        if isinstance(other, Fraction):
+            return self.sign * self.num / self.denom <= other.sign * other.num / other.denom
+        elif isinstance(other, int):
+            return self.sign * self.num / self.denom <= other
+        else:
+            raise TypeError
+
+    def __ge__(self, other):
+        if isinstance(other, Fraction):
+            return self.sign * self.num / self.denom >= other.sign * other.num / other.denom
+        elif isinstance(other, int):
+            return self.sign * self.num / self.denom >= other
+        else:
+            raise TypeError
